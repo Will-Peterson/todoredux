193111252248bfa2cc7228d5ade6a8861543df92
Will-Peterson <william.from.ca@gmail.com>
2021-05-17 20:12:37 -0700

    feat: updated and removed unnecessary files

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {v4} from 'uuid';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 const Form = ({inputProject, setInputProject, projects, setProjects}) => {
 
@@ -26,6 +29,18 @@ const Form = ({inputProject, setInputProject, projects, setProjects}) => {
 
     return (
         <>
+
+<InputGroup className="mb-3">
+    <FormControl
+      placeholder="todo"
+      aria-label="todo"
+      aria-describedby="basic-addon2"
+    />
+    <InputGroup.Append>
+      <Button variant="outline-secondary">Button</Button>
+    </InputGroup.Append>
+  </InputGroup>
+
             <form noValidate autoComplete="off" onSubmit={submitProjectInput}>
             <textfield
                 inputProps={{maxLength: 30}}
